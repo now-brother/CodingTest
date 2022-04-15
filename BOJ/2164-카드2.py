50a681adab53499f2b40fdcc837b2612fe936619
now-brother <nomagold57@gmail.com>
2022-04-15 16:15:21 +0900

    Add: 2022-04-15

--- BOJ/2164-카드2.py
+++ BOJ/2164-카드2.py
@@ -1,15 +1,19 @@
 from collections import deque
 
 n = int(input())
-queue = deque()
-
-for i in range(n):
-    queue.append(i)
-
-while True:
-    queue.popleft()
-    queue.append[queue[0]]
-    queue.popleft()
-    if len(queue) == 1:
-        print(queue[0])
-        break
+queue = deque([])
+if n == 1:
+    print(1)
+else:
+    for i in range(n):
+        queue.append(i + 1)
+    while True:
+        queue.popleft()
+        if len(queue) == 1:
+            print(queue[0])
+            break
+        queue.append(queue[0])
+        queue.popleft()
+        if len(queue) == 1:
+            print(queue[0])
+            break
