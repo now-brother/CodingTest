83126f3018eb000d8e9e5cdd527022395bf79b3c
now-brother <101001148+now-brother@users.noreply.github.com>
2022-04-27 22:30:59 +0900

    Modify:2022-04-27

--- BOJ/1212-2진수8진수.py
+++ BOJ/1212-2진수8진수.py
@@ -1,3 +1,21 @@
-n = int(input(),8)
+# n = int(input(),8)
 
-print('{:#b}'.format(n)[2:])
+# print('{:#b}'.format(n)[2:])
+
+n = list(map(int, list(input())))
+sum = 0
+arr = []
+for i in range(len(n)):
+    sum += ((8**i)*n[len(n)-i-1])
+
+while sum > 0:
+    if sum % 2 == 1:
+        sum = sum // 2
+        arr.append(1)
+    else:
+        sum = sum // 2
+        arr.append(0)
+
+arr.reverse()
+
+print(''.join(map(str, arr)))
